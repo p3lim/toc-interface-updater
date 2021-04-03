4925e61631bf7254c34c0c5e9a3922069fee2a68
Adrian L Lange <mail@p3lim.net>
2021-04-03 18:10:51 +0200

    Comments and output

--- update.sh
+++ update.sh
@@ -9,6 +9,7 @@
 	exit 1
 }
 
+# query CurseForge for the latest game version (e.g. 9.0.5)
 gameVersions="$(curl -sSLH"X-API-Token: $CF_API_KEY" "https://wow.curseforge.com/api/game/versions")"
 if jq '.errorCode' <<< "$gameVersions" 2>/dev/null; then
 	# if this doesn't fail then we have an error
@@ -19,6 +20,7 @@ elif [[ -z "$gameVersions" ]]; then
 	exit 1
 fi
 
+# map game versions to variables
 retailVersion="$(jq -r 'map(select(.gameVersionTypeID == 517)) | max_by(.id) | .name' <<< "$gameVersions")"
 classicVersion="$(jq -r 'map(select(.gameVersionTypeID == 67408)) | max_by(.id) | .name' <<< "$gameVersions")"
 
@@ -27,6 +29,7 @@ if [[ -z "$retailVersion" ]] || [[ -z "$classicVersion" ]]; then
 	exit 1
 fi
 
+# query WoWInterface for Interface version (e.g. 90005)
 interfaceVersions="$(curl -sSLH"X-API-Token: $WOWI_API_TOKEN" "https://api.wowinterface.com/addons/compatible.json")"
 if jq '.ERROR' <<< "$interfaceVersions" 2>/dev/null; then
 	# if this doesn't fail then we have an error
@@ -37,6 +40,7 @@ elif [[ -z "$interfaceVersions" ]]; then
 	exit 1
 fi
 
+# map interface version to variables based on game version
 retailInterfaceVersion="$(jq -r --arg v "$retailVersion" '.[] | select(.id == $v) | .interface' <<< "$interfaceVersions")"
 classicInterfaceVersion="$(jq -r --arg v "$classicVersion" '.[] | select(.id == $v) | .interface' <<< "$interfaceVersions")"
 
@@ -45,9 +49,12 @@ if [[ -z "$retailInterfaceVersion" ]] || [[ -z "$classicInterfaceVersion" ]]; th
 	exit 1
 fi
 
+# write to TOC files
 while read -r file; do
 	# TODO: check/output if we actully made any changes
 	sed -ri 's/^(## Interface: ).*$/\1'"$retailInterfaceVersion"'/' "$file"
 	sed -ri 's/^(## Interface-Retail: ).*$/\1'"$retailInterfaceVersion"'/' "$file"
 	sed -ri 's/^(## Interface-Classic: ).*$/\1'"$classicInterfaceVersion"'/' "$file"
+
+	echo "Updated $file"
 done < <(find . -name '*.toc')
